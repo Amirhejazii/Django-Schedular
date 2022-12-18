5f268673111c55cba767fd41bf6c5f0eef516248
amir hejazi <hejaziamirmohammad5@gmail.com>
2022-12-18 15:22:35 +0330

    initial

---
+++ task/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+from user.models import User
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_to_send = models.DateTimeField()
+    precondition_tasks = models.ManyToManyField('self', blank=True)
+    
+    def __str__(self):
+        return self.title
+    
