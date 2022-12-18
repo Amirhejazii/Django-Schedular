b069d776599f142bdfea7c8b6c1bc4b8987ca19a
amir hejazi <hejaziamirmohammad5@gmail.com>
2022-12-18 16:23:26 +0330

    add registeration with django user

--- task/models.py
+++ task/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 # Create your models here.
-from user.models import User
+from django.contrib.auth.models import User
+
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
--- user/models.py
+++ user/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
-USER_PERMISSION_CHOICES=(
-    ('A','Admin'),
-    ('N','normal')
-)
-
-class User(AbstractUser):
-    permissions = models.PositiveSmallIntegerField(choices=USER_PERMISSION_CHOICES, null=False)
